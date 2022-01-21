a9cc57385b376dce67eb769106f8853ed1db4b8f
Diego Granados <diego@codeando.edu.pe>
2022-01-21 14:10:34 -0400

    primera version

---
+++ app.js
@@ -0,0 +1,36 @@
+const getSleepHours = day => {
+    day = day.toLowerCase();
+      if (day === 'monday') {
+      return 8;
+    } else if (day === 'tuesday') {
+      return 8;
+    } else if (day === 'wednesday') {
+      return 8;
+    } else if (day === 'thursday') {
+      return 8;
+    } else if (day === 'friday') {
+      return 8;
+    } else if (day === 'saturday') {
+      return 8;
+    } else if (day === 'sunday') {
+      return 8;
+    } else {
+      return 'Error!, introduce a correct day'
+    }
+  }
+  ;
+  
+  const getActualSleepHours = () => 
+  getSleepHours('Monday') + getSleepHours('Sunday') + getSleepHours('Tuesday') + getSleepHours('Wednesday') + getSleepHours('Thursday') + getSleepHours('Friday') + getSleepHours('Saturday');
+  
+    const getIdealSleepHours = () => {
+      const idealHours = 7.5;
+      return idealHours *7;
+    }
+  
+  const calculateSleepDebt = () => {
+    const actualSleepHours = getActualSleepHours();
+    const idealSleepHours = getIdealSleepHours();
+  }
+  
+  
