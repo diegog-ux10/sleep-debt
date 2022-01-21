41faba5bbe13a781c9757c26000795c5285ee70b
Diego Granados <diego@codeando.edu.pe>
2022-01-21 16:43:11 -0400

    Done

--- app.js
+++ app.js
@@ -23,14 +23,22 @@ const getSleepHours = day => {
   const getActualSleepHours = () => 
   getSleepHours('Monday') + getSleepHours('Sunday') + getSleepHours('Tuesday') + getSleepHours('Wednesday') + getSleepHours('Thursday') + getSleepHours('Friday') + getSleepHours('Saturday');
   
-    const getIdealSleepHours = () => {
-      const idealHours = 7.5;
-      return idealHours *7;
+    const getIdealSleepHours = hours => {
+      const idealHours = hours;
+      return hours * 7;
     }
   
   const calculateSleepDebt = () => {
     const actualSleepHours = getActualSleepHours();
-    const idealSleepHours = getIdealSleepHours();
-  }
+    const idealSleepHours = getIdealSleepHours(8);
+    if (actualSleepHours === idealSleepHours) {
+      console.log ('Got perfect amount of sleep');
+    } else if (actualSleepHours > idealSleepHours) {
+      console.log ('You got ' + (actualSleepHours - idealSleepHours)  + ' hours more than you needed.');
+    } else if (actualSleepHours < idealSleepHours) {
+      console.log ('You should sleep ' + (idealSleepHours - actualSleepHours) + ' hours more, get some rest');
+    }
+  } 
   
+  calculateSleepDebt();
   
